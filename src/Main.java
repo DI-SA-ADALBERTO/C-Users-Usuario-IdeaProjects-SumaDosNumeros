5cc4bbe618023cb8d508e6e1bc0075157cee01c0
Adalberto Díaz <21670125@itiguala.edu.mx>
2023-02-09 07:52:32 -0600

    primer comit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1=0, num2=0, resultado=0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero de la suma ");
+        num1 = sc.nextInt();
+
+        System.out.println("Ingrese el segundo numero de la suma ");
+        num2 = sc.nextInt();
+
+        System.out.println("El resultado de la suma es " + (num1+num2));
+
+
+    }
+}
